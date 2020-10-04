81539e36382c4fdacd12739f023662f9673d577c
Tarpelite <tarpelite_chan@foxmail.com>
2020-10-04 12:08:54 +0800

    debug

--- s2s-ft/s2s_ft/modeling.py
+++ s2s-ft/s2s_ft/modeling.py
@@ -637,8 +637,14 @@ class BertForRetrieval(BertPreTrainedForSeq2SeqModel):
         relevant_doc_embeds = torch.stack([torch.from_numpy(self.doc_embeds[x]) for x in I]).to(input_ids.device)
         relevant_distance = torch.bmm(relevant_doc_embeds, pooler_output.unsqueeze(-1)).squeeze(-1) # do Inner Product
         relevant_scores = self.softmax(relevant_distance)
-        
-        relevant_doc_features = [torch.tensor(self.features[x].tolist()).to(input_ids.device) for x in I]
+        relevant_doc_features = []
+        for x in I:
+            print(x)
+            print(self.features[x])
+            res = torch.tensor(self.features[x].tolist())
+            print(res)
+            relevant_doc_features.append(res)
+        # relevant_doc_features = [torch.tensor(self.features[x].tolist()).to(input_ids.device) for x in I]
         return relevant_scores, I, relevant_doc_features
 
 
