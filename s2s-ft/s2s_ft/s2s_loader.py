80032fb12bbaeb5685ab518f3ad6339812a5c660
Tarpelite <tarpelite_chan@foxmail.com>
2020-10-06 13:27:07 +0800

    debug

--- s2s-ft/s2s_ft/s2s_loader.py
+++ s2s-ft/s2s_ft/s2s_loader.py
@@ -6,7 +6,7 @@ import math
 import logging
 import torch
 import torch.utils.data
-from pudb import set_trace
+# from pudb import set_trace
 
 logger = logging.getLogger(__name__)
 
@@ -222,7 +222,7 @@ class DecoderConcator:
             for i in range(max_a_len + 2, max_len_in_batch):
                 position_ids.append(i - (max_a_len + 2) + len(tokens_a) + 2)
 
-            set_trace()
+            # set_trace()
             # Token Indexing
             input_ids = self.indexer(tokens)
             # Zero Padding
